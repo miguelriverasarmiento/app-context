a17af35d11c4356f3660310e602f525f63b77dc9
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2022-09-12 18:17:34 -0500

    Comparison with three examples, useState, useContext and useReducer

--- pages/counter/counter-use-state.js
+++ pages/counter/counter-use-state.js
@@ -1,4 +1,31 @@
+import { useState } from 'react';
+
+const Container = ({ children }) => (
+    <div>
+        {children}
+    </div>
+);
+
+const AddOneButton = ({ setCounter }) => (
+    <div>
+        <button onClick={() => setCounter((v) => v + 1)}>Add One</button>
+    </div>
+);
+
+const Counter = ({ counter }) => (
+    <div>Counter: { counter }</div>
+)
+
 export default function CounterUseState() {
 
-    return <div>Hi!</div>
+    const [counter, setCounter] = useState(0);
+
+    return(
+        <div>
+            <Container>
+                <AddOneButton setCounter={setCounter} />
+            </Container>
+            <Counter counter={counter} />
+        </div>
+    );
 }
